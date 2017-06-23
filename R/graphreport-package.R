842bd19539f89e9c4b334ecaf7b345bcd3737568
Andreas Petutschnig <andreas@petutschnig.de>
2017-06-23 10:18:56 +0200

    add travis ci

--- R/graphreport-package.R
+++ R/graphreport-package.R
@@ -2,7 +2,7 @@
 #'
 #' Generates reports about graph size, connectivity and so on of a given graph.
 #'
-#' @name graphreport
+#' @name graphreport-package
 #' @docType package
 #' @importFrom Rcpp evalCpp
 #' @useDynLib graphreport, .registration = TRUE
