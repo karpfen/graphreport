d9479868a7b894a78198b4f74b3e0f13b26056fe
Andreas Petutschnig <andreas@petutschnig.de>
2017-06-26 13:25:28 +0200

    add tests

--- tests/testthat/test_stats.R
+++ tests/testthat/test_stats.R
@@ -1,56 +1,66 @@
 test_that ("graph_sizes", {
-    n <- 50
-    from <- as.integer (runif (min = 0, max = 100, n))
-    to <- as.integer (runif (min = 0, max = 100, n))
+    from <- c (1, 1, 4, 6)
+    to <- c (2, 3, 5, 7)
     graph <- data.frame (from, to)
-    sizes <- get_graph_sizes (graph)
-    testthat::expect_type (sizes, "list")
+    graph_err <- data.frame (from, from)
+
     testthat::expect_error (get_graph_sizes (n),
                             "graph must be of type data.frame.")
-    graph_err <- data.frame (from, from)
     testthat::expect_error (get_graph_sizes (graph_err),
         "graph must contain columns from and to.")
+
+    sizes <- get_graph_sizes (graph)
+    testthat::expect_type (sizes, "list")
+    testthat::expect_equal (sizes$Size, c (3, 2))
+    testthat::expect_equal (sizes$Frequency, c (1, 2))
 })
 
 test_that ("node_degrees", {
-    n <- 50
-    from <- as.integer (runif (min = 0, max = 100, n))
-    to <- as.integer (runif (min = 0, max = 100, n))
+    from <- c (1, 1, 4, 6)
+    to <- c (2, 3, 5, 7)
     graph <- data.frame (from, to)
-    degrees <- get_node_degrees (graph)
-    testthat::expect_type (degrees, "list")
+    graph_err <- data.frame (from, from)
+
     testthat::expect_error (get_node_degrees (n),
                             "graph must be of type data.frame.")
-    graph_err <- data.frame (from, from)
     testthat::expect_error (get_node_degrees (graph_err),
         "graph must contain columns from and to.")
+
+    degrees <- get_node_degrees (graph)
+    testthat::expect_type (degrees, "list")
+    testthat::expect_equal (sum (degrees$frequency == 1), 6)
+    testthat::expect_equal (sum (degrees$frequency == 2), 1)
 })
 
 test_that ("edge_weight", {
-    n <- 50
-    from <- as.integer (runif (min = 0, max = 100, n))
-    to <- as.integer (runif (min = 0, max = 100, n))
-    weight <- runif (n)
+    from <- c (1, 1, 4, 6)
+    to <- c (2, 3, 5, 7)
+    weight <- c (1, 1, 2, 3)
     graph <- data.frame (from, to, weight)
-    weight <- get_edge_weight_summary (graph)
-    testthat::expect_type (weight, "list")
+    graph_err <- data.frame (from, to)
+
     testthat::expect_error (get_edge_weight_summary (n),
                             "graph must be of type data.frame.")
-    graph_err <- data.frame (from, to)
     testthat::expect_error (get_edge_weight_summary (graph_err),
         "graph must contain columns from, to and weight.")
+
+    weight <- get_edge_weight_summary (graph, n = 2)
+    testthat::expect_type (weight, "list")
+    testthat::expect_equal (weight$Value, c (1.5, 3))
 })
 
 test_that ("unique_vertices", {
-    n <- 50
-    from <- as.integer (runif (min = 0, max = 100, n))
-    to <- as.integer (runif (min = 0, max = 100, n))
-    graph <- data.frame (from, to)
-    n_vert <- get_num_unique_vertices (graph)
-    testthat::expect_type (n_vert, "integer")
+    from <- c (1, 1, 4, 6)
+    to <- c (2, 3, 5, 7)
+    weight <- c (1, 1, 2, 3)
+    graph <- data.frame (from, to, weight)
+    graph_err <- data.frame (from, from)
+
     testthat::expect_error (get_num_unique_vertices (n),
                             "graph must be of type data.frame.")
-    graph_err <- data.frame (from, from)
     testthat::expect_error (get_num_unique_vertices (graph_err),
         "graph must contain columns from and to.")
+
+    n_vert <- get_num_unique_vertices (graph)
+    testthat::expect_type (n_vert, "integer")
 })
